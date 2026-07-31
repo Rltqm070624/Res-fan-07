b3d7e2bc7b4f99ee2b1600e66311b865b5338e66
Senchart <308652976+Rltqm070624@users.noreply.github.com>
2026-07-31 11:28:23 +0900

    Update fanchant_data.js

--- fanchant/fanchant_data.js
+++ fanchant/fanchant_data.js
@@ -1,1 +1,1022 @@
-const FANCHANT_DATA = [];
+const FANCHANT_DATA = [
+    {
+        album: "싱글 1집 · Re:Scene",
+        song: "UhUh",
+        vid: "s1S-lnU-yMI",
+        lyrics:
+`**(0:02~) 원-이! 리-브! 미-나미! 메-이! 제-나! 리센느!**
+
+I see 넌 부정하지만
+Now got your **attention**
+I see through ya **(Ya)**
+달콤함 속에 끌려 넌
+아무도 보이지 않는 곳에서
+Feel your **tension**
+Let you know ya **(Ya)**
+Not easy to find me baby
+
+Cuz I 저 깊은 곳에서 피어나려고 해
+몰랐겠지 넌 **Don't even know that**
+꿈에서도 넌 **Don't even know that**
+이젠 시간이 된 거야 향기로 채울래
+Uh, so wait a minute, take it slow
+**Woo baby**
+
+눈을 감아 느껴봐 Feel the chemistry
+**You gonna shout it out Uh Uh**
+**You gonna shout it out**
+그 향기를 따라
+시작된 Sonata 빠져들어
+**Do you wanna follow me?**
+
+Uh Uh Uh Uh Uh Uh **(Uh! Uh!)**
+Uh Uh Uh Uh Uh Uh
+You've got to see the light
+Uh Uh Uh Uh Uh Uh **(Uh! Uh!)**
+Uh Uh Uh Uh Uh Uh
+Too late you can't deny
+
+이미 흘러간 저 **빛 빛 빛**
+사이 꼭꼭 숨겨진 **My scent of scene**
+Now 닫힌 문을 열어 Uh
+You see 타오르는 내 **Flower**
+
+**Bararam bararam bararam (Hey!)**
+향기를 타고 Just ride **(Ride)**
+I'll show you that what is about **(Wow)**
+Imagine that when you find out **Oh!**
+
+Bararam bararam bararam
+원한다면 Follow me **(Me)**
+
+Uh Uh Uh Uh Uh Uh **(Uh! Uh!)**
+Uh Uh Uh Uh Uh Uh
+I'll show you what I mean
+
+이미 너는 **(UhUh)**
+빠져들어 **(UhUh)**
+도망치려 해도 Nuh Uh
+
+꺼져버린 **Burning flower** 속에
+내 향기 남아 꿈처럼 RESCENE **(리! 센! 느!)**
+Uh, wait a minute, take it slow
+**Woo baby**
+
+눈을 감아 느껴봐 Feel the chemistry
+**You gonna shout it out Uh Uh**
+**You gonna shout it out**
+그 향기를 따라
+시작된 Sonata 빠져들어
+**Do you wanna follow me?**
+
+Uh Uh Uh Uh Uh Uh **(Uh! Uh!)**
+Uh Uh Uh Uh Uh Uh
+You've got to see the light
+Uh Uh Uh Uh Uh Uh **(Uh! Uh!)**
+Uh Uh Uh Uh Uh Uh
+Too late you can't deny
+
+이미 흘러간 저 **빛 빛 빛**
+사이 꼭꼭 숨겨진 **My scent of scene**
+Now 닫힌 문을 열어 Uh
+You see 타오르는 내 **Flower**
+
+**Bararam bararam bararam (Hey!)**
+향기를 타고 Just ride **(Ride)**
+I'll show you that what is about **(Wow)**
+Imagine that when you find out **Oh!**
+
+Bararam bararam bararam
+원한다면 Follow me **(Me)**
+
+Uh Uh Uh Uh Uh Uh **(Uh! Uh!)**
+Uh Uh Uh Uh Uh Uh
+I'll show you what I mean
+
+저 빛 속에 환히
+타오르는 꽃이
+재가 되어 다 흩날려도
+영원히 지워지지 않을 The scent
+
+그 향기로 Paint the scene
+다시 그려가 Our scene
+Yeah, the scene will be forever
+**(리! 센! 느!)**
+
+**Uh Uh Uh Uh Uh Uh**
+**Uh Uh Uh Uh Uh Uh**
+I'll show you what I mean
+
+**Bararam bararam bararam (Hey!)**
+향기를 타고 Just ride **(Ride)**
+I'll show you that what is about **(Wow)**
+Imagine that when you find out **Oh!**
+
+Bararam bararam bararam
+원한다면 Follow me **(Me)**
+
+**Uh Uh Uh Uh Uh Uh**
+**Uh Uh Uh Uh Uh Uh**
+I'll show you what I mean`
+    },
+    {
+        album: "싱글 1집 · Re:Scene",
+        song: "YoYo",
+        vid: "CiY40Jh4dws",
+        lyrics: ""
+    },
+    {
+        album: "미니 1집 · SCENEDROME",
+        song: "LOVE ATTACK",
+        vid: "xn12KH78Dx4",
+        lyrics:
+`**Feeling love attack!**
+& **야호!**
+
+I am all you need
+I am all you need
+**(RESCENE)**
+
+So come and get it now
+
+I am all you need
+I am all you need
+**(LOVE ATTACK!)**
+
+Yeah get it
+
+걸음마다 눈부신 나**(너!)**
+Let's get it **started**
+모든 순간 시선을 난
+피하지 **않지**
+
+What you waiting for
+I'm just **waiting for**
+
+나를 **스친**
+깊은 **끌림**
+직감을 Chasing
+
+Oh so strong
+너를 느껴 난
+Yeah so strong
+다가가 이미 난
+On and on
+너를 파헤쳐
+
+**1234**
+
+Like me, Like me
+아주 눈이 부신
+너를 숨김없이 보여줘 **(RESCENE!)**
+
+한 번도 빛난 적 없었던
+미지의 향으로
+온 세상을 물들여 **(LOVE ATTACK!)**
+
+새로워진 장면에 **(SCENE!)**
+두 눈앞은 황홀해 **(RESCENE!)**
+
+너의 손을 잡을 땐
+너와 어우러질 땐
+빛을 이끌어 **With me, With me**
+마치 Chemically 우린
+완벽하게 어울려
+
+**Feeling love attack!**
+
+너 역시 날 많이 기다려 왔겠지
+자 오늘 이 순간 날 You can't deny it
+i got you the felling it's hotter than fire
+U with it, i'm with it, Admit, No lying
+
+한순간에 날 발견해
+가려졌던 널 찾아내
+짙게 번지는 **Ambergris**
+어서 너의 모든 걸 **드러내**
+
+**Beautiful**
+너를 느껴 난
+**Fabulous**
+다가가 이미 난
+**On and on**
+너를 파헤쳐
+
+**1234**(때를 기다려)
+
+Like me, Like me
+아주 눈이 부신
+너를 숨김없이 보여줘 **(RESCENE!)**
+
+한 번도 빛난 적 없었던
+미지의 향으로
+온 세상을 물들여 **(LOVE ATTACK!)**
+
+새로워진 장면에 **(SCENE!)**
+두 눈앞은 황홀해 **(RESCENE!)**
+
+너의 손을 잡을 땐
+너와 어우러질 땐
+빛을 이끌어 **With me, With me**
+마치 Chemically 우린
+완벽하게 어울려
+
+**Feeling love attack!**
+
+**I am all you need**
+**I am all you need**
+
+So come and get it now
+
+**I am all you need**
+**I am all you need**
+
+So come and get it now
+
+1234 **(함성)**
+
+Like me, Like me
+아주 눈이 부신
+너를 숨김없이 보여줘 **(RESCENE!)**
+
+한 번도 빛난 적 없었던
+미지의 향으로
+온 세상을 물들여 **(LOVE ATTACK!)**
+
+새로워진 **장면에**(그 장면에)
+두 눈앞은 **황홀해**(난 황홀해)
+
+너의 손을 **잡을 땐**
+너와 **어우러질 땐**
+
+빛을 이끌어 **With me, With me**
+마치 Chemically 우린
+**완벽하게 어울려**(완벽하게 어울려)
+
+**Feeling love attack!**`
+    },
+    {
+        album: "미니 1집 · SCENEDROME",
+        song: "Pinball",
+        vid: "YyixhiYpkkY",
+        lyrics:
+`(Pinball)
+**(빠져들어 RESCENE!)**
+
+너만 보면 맘이 떨려
+들키기 싫어 훅 멀어져도
+Oh my oh my 심장이 **떨려**
+어질어질 시작된 **Pinball**
+
+들켜서는 안 되는 Game **(Play)**
+끝내고 싶지 않아 Wait **(Wait)**
+순식간에 Highest score
+나 들키기는 싫지만
+Always **down to play**
+
+**(함성)** Ooooh 네 앞에선
+고장 나는 Heart 어색한 걸
+애써 모르는 척
+널 다시 넘겨
+또 난 Holdin' back **nah nah**
+
+I'm playing pinball pinball pinball
+Love is like an arcade (Pin) arcade (Ball)
+들뜬 내 맘이 너에게 닿기 전 (Now) **(RESCENE!)**
+
+Got me going playin' pinball pinball pinball
+혼자만의 Love game 정신없게 난
+계속 해 밀어 온 종일 **(종일!)**
+Playin' pinball **(Pinball!)**
+
+발끝까지 What a feeling
+낯선 듯해 난
+처음 마주한 Love sign **(Oh oh)**
+
+익숙치 않아 그래 다 **(Oh oh)**
+Up down
+
+경고 Beep beep 울린 Flash yeah **(Flash yeah)**
+한 발 더 다가오면 I can't pass yeah **(pass yeah)**
+매일 Level up, Up to the top
+커져가는 이 마음
+
+**(Just like that babe)**
+
+**(함성)** Ooooh 묘한 Pleasure
+이런 내 모습 간지러워
+애써 담담한 척
+널 다시 넘겨
+또 난 Holdin' back **nah nah**
+
+I'm playing pinball pinball pinball
+Love is like an arcade (Pin) arcade (Ball)
+들뜬 내 맘이 너에게 닿기 전 (Now) **(RESCENE!)**
+
+Got me going playin' pinball pinball pinball
+혼자만의 Love game 정신없게 난
+계속 해 밀어 온 종일 **(종일!)**
+Playin' pinball **(Pinball!)**
+
+Pinball game
+끝은 어딘지
+점점 번져가 너의 향기**(RESCENE!)**
+Oooooh yeah
+
+하늘까지 뛰는
+나의 **Heartbeat**
+다 너 때문이지
+Hit it harder
+점점 더 **Closer**
+
+멈출 수가 없는 걸
+I'm falling, I'm falling **(함성)**
+
+We're playing pinball pinball pinball
+Love is like an arcade (Pin) arcade (Ball)
+자꾸 내 맘이 너에게 닿는 걸 (Now) **(RESCENE!)**
+
+Got me going playin' pinball pinball pinball
+어질러진 Love game 정신없게 난
+계속 해 밀어 저 멀리 **(RESCENE!)**
+Playin' pinball **(Pinball)**
+
+**(원이! 리브! 미나미!)**
+Pinball, Pinball
+막을수록 티가 나지
+I'm on a roll
+알 듯 말 듯 맘이 보여
+
+밀어내면 또**(또!)**
+돌아와 Hard to full control
+Won't let go
+
+**(메이! 제나! 리마인!)**
+Pinball, Pinball
+결국엔 너에게 빠질
+나를 알아
+감추려 하지만
+
+네 생각만 나 온종일 **(종일!)**
+Playin' pinball**(Pinball) (함성)**`
+    },
+    {
+        album: "미니 2집 · Glow Up",
+        song: "Glow Up",
+        vid: "-55bUrG1qjg",
+        lyrics:
+`**(원이 리브 미나미 메이 제나 Glow Up!)**
+
+I wanna glow 늘 언제든지
+투명한 이 잔향같이 **(yeah!)**
+아주 은은하게 스며가
+조금씩 Let me show you
+What I **like like like**
+
+햇살같이 맑은 기분
+물결처럼 비친 내 맘
+들여다봐 거긴 뭐가 있을까 **(리마인!)**
+
+맑은 향기가
+살랑 불어와
+길을 나선 걸음엔
+설렘만이 가득해**(가득해!)**
+
+맘은 Moving fast
+나를 재촉해
+I've been waiting too long
+
+**Baby**, we are growing up
+**Lately**, 맘이 일렁여
+**멀리** 발을 떼고서
+날아 **Keep up keep up keep up**
+
+두 눈 속에
+더**(더)** 담고 싶어
+보여줘 **I'm so curious**
+빛을 가득 머금어
+네게 더 반짝일래
+**(리센느!)**
+
+Glow Up
+
+that-that-that-that-that-**Glow Up**
+that-that-that-that-that-**Glow Up**
+that-that-that-that-that-**Glow Up**
+
+반짝일래 **(Glow Up)**
+
+that-that-that-that-that-**Glow Up**
+that-that-that-that-that-**Glow Up**
+that-that-that-that-that-**Glow Up**
+
+거울 속 비친 **(너와 나)**
+You're like a diamond
+눈을 뜬 순간 **(그 순간)**
+숨 막힐 듯 Shining through
+
+맘에 들어 **Like it**
+산뜻 산뜻한 **Vibin'**
+
+이 분위기를 따라
+You're gonna taste with me?
+Let's go right now
+
+자꾸만 살랑 불어와
+감겨오는 손짓에
+맘을 뺏긴 것 같아 **(뻇겼어!)**
+
+낯선 꿈의 이야기로
+더 다가오라고
+계속 나를 불러
+Time has come
+
+**Baby**, we are growing up
+**Lately**, 맘이 일렁여
+**멀리** 발을 떼고서
+날아 **Keep up keep up keep up**
+
+두 눈 속에
+더**(더)** 담고 싶어
+보여줘 **I'm so curious**
+빛을 가득 머금어
+네게 더 반짝일래
+**(리센느!)**
+
+Glow Up
+
+that-that-that-that-that-**Glow Up**
+that-that-that-that-that-**Glow Up**
+that-that-that-that-that-**Glow Up**
+
+반짝일래 **(Glow Up)**
+
+that-that-that-that-that-**Glow Up**
+that-that-that-that-that-**Glow Up**
+that-that-that-that-that-**Glow Up**
+(ah ah ah ah)
+
+반짝일래 **(Glow Up)**
+
+ah ah ah ah
+
+**Glow Up(함성)**`
+    },
+    {
+        album: "싱글 2집 · Dearest",
+        song: "Deja Vu",
+        vid: "9FlQOv6-Mjc",
+        lyrics:
+`**(원이 리브 미나미 메이 제나 / 리센느 짱!)**
+
+햇살 젖은 바람
+잠이 든 I **(I)**
+물든 창문 틈새
+스며든 **light (light)**
+
+나의 코끝을 스친 scent
+**(그 향기에)**
+피어난 작은 보조개
+**(Oh It's so bright)**
+
+책상 위에 그린 낙서
+너와 나눈 그 비밀도**(Deja Vu!)**
+
+바람결에 실려 다시
+되돌아간 기분 **after all**
+
+처음 스친 그때
+이 향길 **기억해줘**
+닿은 그 순간
+펼쳐질 **deja Vu**
+
+같은 꿈을 꾸듯 **(WAH)**
+눈을 감아보면 **(YEAH)**
+익숙한 **deja Vu**
+**Oh oh oh ha**
+
+I I I I I
+Yeah it's like a **deja Vu**
+You and I
+다시 닿을 수 없다 해도
+
+같은 꿈을 꾸듯 **(WAH)**
+눈을 감아보면 **(YEAH)**
+익숙한 **deja Vu**
+**Oh oh oh ha**
+
+빽빽한 책장 사이
+손때 묻은 한 칸
+바랜 책 모퉁일
+넘기면 튀어나온
+
+자그만 이야길 들어
+**(귀 기울여 봐)**
+조금은 서툴렀던 날
+
+빛이 바랜 쪽지 속에
+나를 어루만진 네 voice **(Deja Vu!)**
+
+바람결에 실려 다시
+되돌아간 기분 **after all**
+
+처음 스친 그때
+이 향길 **기억해줘**
+닿은 그 순간
+펼쳐질 **deja Vu**
+
+같은 꿈을 꾸듯 **(WAH)**
+눈을 감아보면 **(YEAH)**
+익숙한 **deja Vu**
+**Oh oh oh ha**
+
+I I I I I
+Yeah it's like a **deja Vu**
+You and I
+다시 닿을 수 없다 해도
+
+같은 꿈을 꾸듯 **(WAH)**
+눈을 감아보면 **(YEAH)**
+익숙한 **deja Vu**
+**Oh oh oh ha**
+
+**(기억할게!)**
+(I've been thinking about you)
+그리워질 너와 나
+**(기억해줘!)**
+(I've been dreaming about you)
+이 순간을 잊지 마
+
+언젠가
+세상 끝에서
+마주할 날
+오랜 **deja Vu**
+
+같은 꿈을 꾸듯 **(WAH)**
+눈을 감아보면 **(YEAH)**
+익숙한 **deja Vu**
+**Oh oh oh ha**
+
+I I I I I
+Yeah it's like a **deja Vu**
+You and I
+다시 닿을 수 없다 해도
+
+같은 꿈을 꾸듯 **(WAH)**
+눈을 감아보면 **(YEAH)**
+익숙한 **deja Vu**
+**Oh oh oh ha(함성)**`
+    },
+    {
+        album: "미니 3집 · lip bomb",
+        song: "Heart Drop",
+        vid: "VKlrVbgJG-g",
+        lyrics:
+`Can't stop, won't stop
+We can make your **heart drop**
+
+Can't stop, won't stop
+We can make your heart
+
+Can't stop, won't stop
+We can make your **heart drop**
+
+Can't stop, can't stop, can't stop **(리센느!)**
+
+메말랐던 너의 하루에
+그 한순간에 **Melt away, melt away**, baby
+퍼져드는 향기에 널 물들여
+**Slip away, slip away**, uh uh uh baby
+
+네 귓속을 간지럽힌 **Hypersonic**
+빠져드는 Rhythm 부드럽게 번져와
+느껴진 Delight **(느껴진 Delight)**
+손을 뻗어 Get this feeling
+
+Can't stop, won't stop
+We can make your **heart drop**
+
+Can't stop, won't stop
+We can make your heart
+
+Can't stop, won't stop
+We can make your **heart drop**
+
+Can't stop, won't stop
+We can make your heart drop
+
+내게 더 빠져드는 Scent **(빠져들어 리센느 사랑해!)**
+이 떨림은 Just for you
+서서히 스며들어
+넌 이미 **Can't stop, won't stop**
+Make your heart drop
+
+Already told ya I'm all you need
+닿은 그 순간 빠져들지
+선명한 Voices I'm all you
+(Just take me to the next part right now)
+
+조심스**레** 물들여 **끝도 없이**
+은은하**게** 퍼지는 **Melody에**
+한 번 더 Delight **(한 번 더 Delight)**
+손을 뻗어 Get this feeling
+
+Can't stop, won't stop
+We can make your **heart drop**
+
+Can't stop, won't stop
+We can make your heart
+
+Can't stop, won't stop
+We can make your **heart drop**
+
+Can't stop, won't stop
+We can make your heart drop
+
+내게 더 빠져드는 Scent **(빠져들어 리센느 사랑해!)**
+이 떨림은 Just for you
+서서히 스며들어
+넌 이미 **Can't stop, won't stop**
+Make your heart drop
+**(함성)**
+
+멈추지 못할 달콤한 Hum
+방심한 새에 퍼진 Light
+Heart dropping 가식 없이 넌
+이대로 빠져들어 더
+네 맘에 새긴 **잊지 못할 Heart drop**
+
+내게 더 빠져드는 Scent **(빠져들어 리센느 사랑해!)**
+이 떨림은 Just for you
+점점 더 잠겨 들어
+넌 이미 **Can't stop, won't stop**
+Make your heart drop
+
+Can't stop, won't stop
+**We can make your heart**
+Can't stop, won't stop
+**We can make your heart**
+Can't stop, won't stop
+**We can make your heart**
+Can't stop, won't stop
+We can make your heart drop
+
+Can't stop, won't stop
+We can make your heart
+Can't stop, won't stop
+We can make your heart
+Can't stop, can't stop **(함성)**`
+    },
+    {
+        album: "미니 3집 · lip bomb",
+        song: "Bloom",
+        vid: "Ma6IENHO584",
+        lyrics:
+`완벽한 moment 나다운 arise
+더 망설임 없이 더 밝게 빛나
+이 순간 진심을 담아 **(아)**
+나를 그려갈게 난 **(난)**
+한편의 movie를 다 채워가도록 **(리센느!)**
+
+**(이젠)** 내가 날 비춰줄래
+**(내게)** 한발 다가갈래
+따스히 hands on me now
+
+Got me feelin'
+기다려 왔던 작은 빛이
+내 안을 비추면
+서서히 bloom, ah
+
+**(날 깨워내)**
+내 안의 꿈을 깨운 듯이
+새로워지는 heart
+가득히 bloom, ah
+더 아름답게 피어나지
+
+**(ah ah)** Look into my eyes
+**(ah ah)** 눈이 부신 you
+**(ah ah)** 뭐든 you can do
+It's time to shine my bloom
+
+넌 찡그렸던 표정을 펴 **(no)**
+조급해할 필요 없어 **(slow)**
+입술엔 lip balm 살짝 바르고
+We do it in our time, we go where we wanna go
+For you 시작된 이 하루는
+**(널 위한 거야)**
+You can try 뭐든 whatever you want **(리센느!)**
+
+**(이젠)** 내가 날 비춰줄래
+**(내게)** 한발 다가갈래
+따스히 hands on me now
+
+Got me feelin'
+기다려 왔던 작은 빛이
+내 안을 비추면
+서서히 bloom, ah
+
+**(날 깨워내)**
+내 안의 꿈을 깨운 듯이
+새로워지는 heart
+가득히 bloom, ah
+더 아름답게 피어나지
+
+**(ah ah)** Look into my eyes
+**(ah ah)** 눈이 부신 you
+**(ah ah)** 뭐든 you can do
+It's time to shine my bloom
+**(함성)**
+
+그 누구도 대신할 수 없는 걸
+매 순간의 주인공인 너와 나
+다른 누구와 비교하지 마
+다 이대로 괜찮아
+나를 믿어 난 **(너를 믿어 리센느!)**
+
+Got me feelin'
+기다려 왔던 작은 빛이
+내 안을 비추면
+서서히 bloom, ah
+
+**(날 깨워내)**
+내 안의 꿈을 깨운 듯이
+새로워지는 heart
+가득히 bloom, ah
+**(더 아름답게 피어나지)**
+
+**(ah ah)** Look into my eyes
+**(ah ah)** 눈이 부신 you
+**(ah ah)** 뭐든 you can do
+더 아름답게 피워내지
+
+**(ah ah)** Look into my eyes
+**(ah ah)** 눈이 부신 you
+**(ah ah)** 뭐든 you can do
+It's time to shine my bloom **(함성)**`
+    },
+    {
+        album: "디지털 싱글 · Busy Boy",
+        song: "Busy Boy",
+        vid: "Cb4yYuTGoFQ",
+        lyrics:
+`(Hey, it's me)
+**(정원이 진경은 미나미 이예빈 김가영 리센느)**
+**(비!지!보!이!)**
+
+I wanna know what's on your mind
+아무렇지 않게 you always make me **(behind)**
+that's just how you do
+지나칠만큼 차가운 넌 또 **(바빠)**
+There you go again, babe
+
+꽉찬 네 하루 틈새 난 마**(치)**
+꼭 희미해진 **(dream)**
+괜찮은 척 don't you know me?
+지웠다 썼다 보내버렸**(지)**
+화가 난 **(emoji)**
+
+I don't know what I should do
+밀었다 당기긴
+이제 그만해 please make sure **((함성))**
+
+넌 불투명한 안개 속 같이
+어느샌가 I lose my way
+난 네가 싫어
+
+Busy, **(busy)**, busy, **(busy)**
+Busy, **(busy)**, busy, **(busy)**
+Busy boy, 네가 싫어 **(Busy boy)**
+
+난 네가 싫어
+Busy, **(busy)**, busy, **(busy)**
+Busy, **(busy)**, busy, **(busy)**
+Busy boy, 네가 싫어 **(Busy boy)**
+
+Boy 넌 아득하게 깊은 the ocean
+(난 매일 그 안을 헤매다)
+애쓰고 싶지 않은데
+**(이럴거면 왜)**
+Why did you say love me? **((함성))**
+
+넌 매일 mess me up babe
+날 그렇게 **(시험해)**
+갈수록 이해할 수 없는 말만 **(늘어놔 왜)**
+
+정답없는 문제 속에서
+You're my only answer, you know
+애써 아닌 척 해도 Oh
+
+난 네가 싫어
+Busy, **(busy)**, busy, **(busy)**
+Busy, **(busy)**, busy, **(busy)**
+Busy boy, 네가 싫어 **(Busy boy)**
+
+난 네가 싫어
+Busy, **(busy)**, busy, **(busy)**
+Busy, **(busy)**, busy, **(busy)**
+Busy boy, 네가 싫어 **(Busy boy)**
+
+**((함성))**
+
+내 머리 속은 Dizzy
+벌써 넌 몇번째
+똑같이 늘어놔 excuse
+**(내 머리 속은 Dizzy)**
+
+꾸밈없는 대답을 난 원해
+투명하게 보여줘 oh
+
+난 네가 싫어
+Busy, **(busy)**, busy, **(busy)**
+Busy, **(busy)**, busy, **(busy)**
+Busy boy, 네가 싫어 **(Busy boy)**
+
+난 네가 싫어
+Busy, **(busy)**, busy, **(busy)**
+Busy, **(busy)**, busy, **(busy)**
+Busy boy, 네가 싫어 **(Busy boy)**
+
+난 네가 싫어
+Busy, busy, busy, busy
+Busy, busy, busy, busy
+Busy boy, 네가 싫어 Busy boy
+
+난 네가 싫어
+Busy, busy, busy, busy
+Busy, busy, busy, busy
+Busy boy, 네가 싫어 **(Busy boy)**
+
+**((함성))**
+**(네!가!좋!아! 리!센!느!)**`
+    },
+    {
+        album: "디지털 싱글 · Runaway",
+        song: "Runaway",
+        vid: "7DZlkZ4bMpU",
+        lyrics:
+`**(0:07~ 원이 리브 미나미 메이 제나)**
+**(리센느 런!어!웨!이!)**
+회색빛에 갇혀버린 **Neverland**
+We know how it ends
+또 의미 없이 돌고도는 **Everyday**
+
+더는 **갈 수 없게**
+여기 **맴돌게 해**
+내가 사라진 꿈속에
+끝없는 **쳇바퀴**
+울리는 **메아리**
+벽을 넘어 Let's break this fate
+
+날 뒤흔드는 **Heart**, now I run away **(런!어!웨!이!)**
+돌아서 난 이미 어서 Hold my hand **(리!센!느!)**
+한걸음 Move
+멀리 저 희미한 View
+푸른 빛을 따라 날
+끌어당겨 넌 Now I run away
+**I run away**
+**(함성)**
+**(끌!어!당!겨! 리!센!느!)**
+
+멈춰버린 시간 속
+한치 앞도 알 수 없는 내일은
+**날 두렵게 해**
+아무것도 할 수 없던 Me and you **(You and me)**
+
+Oh, I try
+**All night**
+심장의 떨림을 쫓아
+우리만의 다른 여정을 찾아 떠나 **(가자~~)**
+
+날 뒤흔드는 Heart, now I run away **(I run away)**
+돌아서 난 이미 어서 Hold my hand **(Hold my hand)**
+한걸음 Move
+멀리 저 희미한 View
+푸른 빛을 따라 날
+끌어당겨 넌 Now I run away
+**I run away**
+
+**(함성)**
+I run, I run away
+난 발을 떼 더 멀리 Now I run away
+**I run, I run away**
+끝을 넘어서 Now I **run away**
+
+맘을 외면한 채
+진심을 삼켜내
+매일 공허한 꿈속에
+끝없는 미로 속
+손을 잡아주길
+끝을 넘어 Let's break this fate
+
+**(함성)**
+날 뒤흔드는 Heart, now I run away **(I run away)**
+돌아서 난 이미 어서 Hold my hand **(I run I run)**
+한걸음 Move
+멀리 저 희미한 View
+푸른 빛을 따라 날
+끌어당겨 넌 Now I run away
+**(I run away)**
+
+날 뒤흔드는 Heart, now I run away
+돌아서 난 이미 어서 Hold my hand
+한걸음 Move
+멀리 저 희미한 View
+푸른 빛을 따라 날
+**끌어당겨 넌 Now I run away**
+I run away
+
+**(함성)**
+**(2:59~ I run I run I run away!)**`
+    },
+    {
+        album: "스페셜 싱글 · Pretty Girl",
+        song: "Pretty Girl",
+        vid: "RX592yMx7P0",
+        lyrics:
+`Yeah yeah **(정원이 진경은 미나미)** yeah yeah
+Yeah yeah yeah **(이예빈 김가영 리센느)** yeah yeah
+**(프리티걸 나야 나)**
+**one two three yeah**
+
+If you wanna pretty
+**every wanna pretty**
+안된다는 맘은
+**no no no no**
+
+If you wanna pretty
+**every wanna pretty**
+어디서나 당당하게 걷기
+
+**(리센느!)**
+
+나와 맞는 옷에 또 받쳐주는 말투
+센스있는 포즈 그냥 되지는 않죠
+
+생활 상식은 **기본** 시사 상식은 **선택**
+다 끊임없는 노력이죠
+
+Girl **(girl!)** Pretty Girl **Pretty Girl**
+조금도 망설일 것 없죠 난 **(넌)**
+Beautiful Girl Beautiful Girl
+Beautiful **yeah yeah yeah yeah**
+
+Girl **(girl!)** Pretty Girl **Pretty Girl**
+그냥 되진 않는거죠 난 **(넌)**
+Beautiful Girl Beautiful
+**Yeah yeah yeah yeah**
+Come on Beautiful Girl
+Yeah yeah **(리마인)** (yeah!)
+
+If you wanna pretty
+**every wanna pretty**
+안된다는 맘은
+**no no no no**
+
+If you wanna pretty
+**every wanna pretty**
+어디서나 당당하게 걷기
+
+Girl **(girl!)** Pretty Girl **Pretty Girl**
+조금도 망설일 것 없죠 난 **(넌)**
+Beautiful Girl Beautiful Girl
+Beautiful **yeah yeah yeah yeah**
+
+Girl **(girl!)** Pretty Girl **Pretty Girl**
+그냥 되진 않는거죠 난 **(넌)**
+Beautiful Girl Beautiful
+**Yeah yeah yeah yeah**
+Come on Beautiful Girl
+
+마음은 예쁘게 **(리)**
+표정은 산뜻하게 **(센)**
+하루를 시작하면서 **(느)**
+잊지 말아야 하죠**(짱)**
+두 눈을 깜박이며 살짝 미소 지으면
+이젠 모든게 완벽하죠 **(함성)**
+
+Girl **(girl!)** Pretty Girl **Pretty Girl**
+누구라도 될 수 있죠 난 **(넌)**
+Beautiful Girl Beautiful
+Yeah yeah yeah yeah
+**Yeah yeah yeah yeah**
+
+Girl **(girl!)** Pretty Girl Pretty Girl
+그냥 되진 않는거죠 난 **(넌)**
+Beautiful Girl Beautiful Girl **(함성)**
+Yeah yeah
+
+**If you wanna pretty**
+**every wanna pretty**
+**안된다는 맘은**
+**no no no no**
+
+**If you wanna pretty**
+**every wanna pretty**
+**어디서나 당당하게 걷기**
+
+Yeah yeah **(눈부셔 빛난다 리센느)** yeah yeah
+Yeah yeah yeah **(고마워 영원해 리마인)** yeah yeah
+**(우리 모두 프리티걸)**
+**(yeah~~~)**`
+    }
+];

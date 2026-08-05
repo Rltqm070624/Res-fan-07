23bfd78f29e34f3b5e7e29445aa6e290b2233e40
rescene-archive <308652976+senarchive@users.noreply.github.com>
2026-08-06 00:29:58 +0900

    Update member_data.js

--- js/member_data.js
+++ js/member_data.js
@@ -1,33 +1,32 @@
-
 const MEMBER_DATA = [
     {
         key: 'woni', nameKo: '원이', nameEn: 'WONI', color: '#f4c95d',
         birthday: '2004.05.25', position: '리더',
-        mbti: 'ESFP', 별명: '파이리', SNS: '-',
+        mbti: 'ESFP', nickname: '파이리', sns: '-',
         intro: '오이쉬'
     },
     {
         key: 'liv', nameKo: '리브', nameEn: 'LIV', color: '#6ec6ff',
         birthday: '2006.10.11', position: '보컬',
-        mbti: 'ESFP', 별명: '리트, 무리브', SNS: '-',
+        mbti: 'ESFP', nickname: '리트, 무리브', sns: '-',
         intro: '너도? 나도!'
     },
     {
         key: 'minami', nameKo: '미나미', nameEn: 'MINAMI', color: '#2b99c4',
         birthday: '2006.11.29', position: '보컬',
-        mbti: 'ENFP', 별명: '갸루귀신', SNS: '안무 창작 · 스티치 성대모사',
+        mbti: 'ENFP', nickname: '갸루귀신', sns: '안무 창작 · 스티치 성대모사',
         intro: '야호~'
     },
     {
         key: 'may', nameKo: '메이', nameEn: 'MAY', color: '#ecd25b',
         birthday: '2008.08.19', position: '보컬',
-        mbti: 'INTP', 별명: '메트, 메라디오', SNS: '-',
+        mbti: 'INTP', nickname: '메트, 메라디오', sns: '-',
         intro: '그립감이 좋다!'
     },
     {
         key: 'zena', nameKo: '제나', nameEn: 'ZENA', color: '#ff6b6b',
         birthday: '2008.11.27', position: '보컬 · 댄스',
-        mbti: 'INFP', 별명: '신라공주, 까엉이', SNS: '-',
+        mbti: 'INFP', nickname: '신라공주, 까엉이', sns: '-',
         intro: '그뤠여?'
     }
 ];
